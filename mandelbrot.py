ab0c48bf12e7ffd30e0e842f5ea6e93413b82b12
kamielgulpen <k.gulpen@hotmail.nl>
2020-11-01 17:15:29 +0100

    Area estimation

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,39 +3,58 @@ import matplotlib.pyplot as plt
 from numpy import newaxis
 
 def mandelbrot_computation(N_max, some_threshold, x, y):
+    """
+    Computes the mandelbrot set. 
+    Takes as initial values: N_max(integer), some_threshold(number), x(array), y(array)
+    Returns a array with boolean values, True when value is part of the mandelbrot set and False if not.
+    """
     c = x[:,newaxis] + 1j*y[newaxis,:]
 
     # Mandelbrot iteration
     z = c
 
-    # The code below overflows in many regions of the x-y grid, suppress
-    # warnings temporarily
+    # Does the mandelbrot iteration N_max times
     for j in range(N_max):
         z = z**2 + c
     
+    # Checks for all the values if value is part of the mandelbrot set
     mandelbrot_set = (abs(z) < some_threshold)
+
     return mandelbrot_set
 
 def compute_image_mandelbrot(N_max, some_threshold, nx, ny):
+    """
+    Computes a image of the mandel brot set.
+    Takes as initial values N_max(integer), some_threshold(Number), nx(integer), ny(integer)
+    """
     # A grid of c-values
     x = np.linspace(-2, 1, nx)
     y = np.linspace(-1.5, 1.5, ny)
     
+    # Makes complex number wit x and y values
     c = x[:,newaxis] + 1j*y[newaxis,:]
 
+    # Computes the mandelbrot with x and y values
     mandelbrot_set = mandelbrot_computation(N_max, some_threshold, x, y)
+
+    # Makes image of mandelbrot set
     plt.imshow(mandelbrot_set.T, extent=[-2, 1, -1.5, 1.5])
     plt.gray()
     plt.show()
 
 
 def compute_area_mandelbrot(N_max, some_threshold, nx, ny):
-    # A grid of c-values
+    """
+    Computes the Area of the mandel brot set with the monte carlo method.
+    Takes as initial values N_max(integer), some_threshold(Number), nx(integer), ny(integer)
+    returns the amount of hits and the ratio of hits/total shots
+    """
+    # Initializes values
     hits = 0
-
     x = np.array([])
     y = np.array([])
 
+    # Makes a x and y array of random values
     for i in range(nx):
         a = np.random.uniform(-2,1)
         x = np.append(x,a)
@@ -43,12 +62,16 @@ def compute_area_mandelbrot(N_max, some_threshold, nx, ny):
         b = np.random.uniform(-1.5,1.5)
         y = np.append(y,b)
 
+    # Uses the random values for the mandelbrot computation
     mandelbrot_set = mandelbrot_computation(N_max, some_threshold, x, y)
+
+    # Checks for all the values if they are in our out of the mandelbrot set
     for row in mandelbrot_set:
         for value in row:
             if value == True:
                 hits+=1
     
+    # calculates the ratio hits/total shots
     ratio = hits/(nx*ny)
 
     return hits, ratio
