981683f5f58a3ab11aebcf242aa791cc83a43969
kamielgulpen <k.gulpen@hotmail.nl>
2020-11-01 17:04:06 +0100

    Area estimation

--- mandelbrot.py
+++ mandelbrot.py
@@ -2,16 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import newaxis
 
-def compute_mandelbrot(N_max, some_threshold, nx, ny):
-    # A grid of c-values
-    x = np.linspace(-2, 1, nx)
-    y = np.linspace(-1.5, 1.5, ny)
-
-
-    print(x[:,newaxis])
+def mandelbrot_computation(N_max, some_threshold, x, y):
     c = x[:,newaxis] + 1j*y[newaxis,:]
 
-    print(c)
     # Mandelbrot iteration
     z = c
 
@@ -19,13 +12,52 @@ def compute_mandelbrot(N_max, some_threshold, nx, ny):
     # warnings temporarily
     for j in range(N_max):
         z = z**2 + c
-    print(abs(z))
+    
     mandelbrot_set = (abs(z) < some_threshold)
-
     return mandelbrot_set
 
-mandelbrot_set = compute_mandelbrot(50, 5., 1000, 1000)
+def compute_image_mandelbrot(N_max, some_threshold, nx, ny):
+    # A grid of c-values
+    x = np.linspace(-2, 1, nx)
+    y = np.linspace(-1.5, 1.5, ny)
+    
+    c = x[:,newaxis] + 1j*y[newaxis,:]
 
-plt.imshow(mandelbrot_set.T, extent=[-2, 1, -1.5, 1.5])
-plt.gray()
-plt.show()
+    mandelbrot_set = mandelbrot_computation(N_max, some_threshold, x, y)
+    plt.imshow(mandelbrot_set.T, extent=[-2, 1, -1.5, 1.5])
+    plt.gray()
+    plt.show()
+
+
+def compute_area_mandelbrot(N_max, some_threshold, nx, ny):
+    # A grid of c-values
+    hits = 0
+
+    x = np.array([])
+    y = np.array([])
+
+    for i in range(nx):
+        a = np.random.uniform(-2,1)
+        x = np.append(x,a)
+    for i in range(ny):
+        b = np.random.uniform(-1.5,1.5)
+        y = np.append(y,b)
+
+    mandelbrot_set = mandelbrot_computation(N_max, some_threshold, x, y)
+    for row in mandelbrot_set:
+        for value in row:
+            if value == True:
+                hits+=1
+    
+    ratio = hits/(nx*ny)
+
+    return hits, ratio
+
+if __name__=="__main__":
+    
+    monte_carlo = compute_area_mandelbrot(50, 5., 100, 10)
+    hits = monte_carlo[0]
+    ratio = monte_carlo[1]
+
+    print(hits, ratio)
+    compute_image_mandelbrot(50, 5., 100, 10)
