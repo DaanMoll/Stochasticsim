78a440585517cb850a201d13906b7f043b209a96
kamielgulpen <k.gulpen@hotmail.nl>
2020-11-02 11:54:42 +0100

    random numbers and lhs

--- mandelbrot.py
+++ mandelbrot.py
@@ -6,25 +6,44 @@ from skopt.space import Space
 from numpy import newaxis
 
 def random_sample(x, y, n_samples):
-    # Makes a x and y array of random values
+    """
+    Takes random n_samples from a uniform distribution.
+    Returns array x and y.
+    """
+
+    # Makes arrays of x and y values
     for i in range(n_samples):
         a = np.random.uniform(-2,1)
         x = np.append(x,a)
         b = np.random.uniform(-1.5,1.5)
         y = np.append(y,b)
+
     return x, y
 
 def lhs_sample(x, y, n_samples):
+    """
+    Takes random n_samples with the lhs method.
+    Returns array x and y.
+    """
+
+    # Makes the space of points which van be chosen from
     space = Space([(-2.,1.), (-1.5, 1.5)])  
+
+    # Chooses which kind oh lhs will be used
     lhs = Lhs(lhs_type="classic", criterion=None)
+
+    # Generates n_samples withhi the chosen space
     coordinates = lhs.generate(space.dimensions, n_samples)
 
+    # appends all x and y values to array
     for coordinate in coordinates:
         a = coordinate[0]
         x = np.append(x,a)
         b = coordinate[1]
         y = np.append(y,b)
+
     return x, y
+
 def mandelbrot_computation(N_max, some_threshold, x, y):
     """
     Computes the mandelbrot set. 
@@ -69,7 +88,7 @@ def compute_image_mandelbrot(N_max, some_threshold, nx, ny):
 def compute_area_mandelbrot(N_max, some_threshold, n_samples, kind):
     """
     Computes the Area of the mandel brot set with the monte carlo method.
-    Takes as initial values N_max(integer), some_threshold(Number), nx(integer), ny(integer)
+    Takes as initial values N_max(integer), some_threshold(Number), n_samples(integer), kind(string = random, lhs)
     returns the amount of hits and the ratio of hits/total shots
     """
 
