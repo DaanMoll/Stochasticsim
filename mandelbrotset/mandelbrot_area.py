f6333bde8d2db9269e6223ed79d73a31fdb0ac99
Daan Moll <daanmoll@hotmail.com>
2020-11-17 11:11:29 +0100

    antithetic

--- mandelbrotset/mandelbrot_area.py
+++ mandelbrotset/mandelbrot_area.py
@@ -5,14 +5,13 @@ from skopt.sampler import Lhs
 from skopt.space import Space
 from numpy import newaxis
 
-from .sampling.lhs import lhs_sample
-from .sampling.ortho import ortho_sample 
-from .sampling.random_sampling import random_sample, half_random_sample
-from .sampling.orthogonal import scale_points
+from .sampling.lhs import lhs_sample, lhs_sample_anti
+from .sampling.random_sampling import random_sample, random_sample_anti
+from .sampling.orthogonal import scale_points, scale_points_anti
 
 from .mandelbrotcompute import mandelbrot_computation
 
-def compute_area_mandelbrot(N_max, some_threshold, n_samples, kind, antithetic):
+def compute_area_mandelbrot(N_max, some_threshold, n_samples, kind):
     """
     Computes the Area of the mandel brot set with the monte carlo method.
     Takes as initial values N_max(integer), some_threshold(Number), n_samples(integer), kind(string = random, lhs, ortho)
@@ -20,12 +19,9 @@ def compute_area_mandelbrot(N_max, some_threshold, n_samples, kind, antithetic):
     """
 
     hits = 0
-    hits_anti = 0
 
     if kind == "Random":
         sample = random_sample(n_samples)
-    elif kind == "Half Random":
-        sample = half_random_sample(n_samples)
     elif kind == "LHS":
         sample = lhs_sample(n_samples)
     elif kind == "Orthogonal":
@@ -49,21 +45,34 @@ def compute_area_mandelbrot(N_max, some_threshold, n_samples, kind, antithetic):
     # total plot area is 9 so ratio * 9
     area = ratio * 9
 
-    if antithetic:
-        
-        x_a = sample[2]
-        y_a = sample[3]
-        # print("kind", kind, x_a, y_a)
-        mandelbrot_set = mandelbrot_computation(N_max, some_threshold, x_a, y_a)
-        
-        for row in mandelbrot_set:
-            for value in row:
-                if value == True:
-                    hits_anti+=1
-
-        ratio_anti = hits_anti/n_samples**2
-        area_anti = ratio_anti * 9
-
-        return hits, ratio, area, hits_anti, ratio_anti, area_anti
-
     return hits, ratio, area
+
+def compute_area_mandelbrot_anti(N_max, some_threshold, n_samples, kind):
+    """
+    Computes the Area of the mandel brot set with the monte carlo method.
+    Takes as initial values N_max(integer), some_threshold(Number), n_samples(integer), kind(string = random, lhs, ortho)
+    returns the amount of hits and the ratio of hits/total shots
+    """
+    hits_anti = 0
+
+    if kind == "Random":
+        sample = random_sample_anti(n_samples)
+    elif kind == "LHS":
+        sample = lhs_sample_anti(n_samples)
+    elif kind == "Orthogonal":
+        sample = scale_points_anti(n_samples)
+
+    x_a = sample[2]
+    y_a = sample[3]
+
+    mandelbrot_set = mandelbrot_computation(N_max, some_threshold, x_a, y_a)
+    
+    for row in mandelbrot_set:
+        for value in row:
+            if value == True:
+                hits_anti+=1
+
+    ratio_anti = hits_anti/n_samples**2
+    area_anti = ratio_anti * 9
+
+    return hits_anti, ratio_anti, area_anti
--- mandelbrotset/sampling/lhs.py
+++ mandelbrotset/sampling/lhs.py
@@ -24,22 +24,49 @@ def lhs_sample(n_samples):
 
     coordinates = 0
     # Generates n_samples withhi the chosen space
-    if antithetic:
-        coordinates = lhs.generate(space.dimensions, int(n_samples/2))
-    else:
-        coordinates = lhs.generate(space.dimensions, n_samples)
+    coordinates = lhs.generate(space.dimensions, n_samples)
     
     # appends all x and y values to array
     for coordinate in coordinates:
         a = coordinate[0]
         x = np.append(x,a)
         b = coordinate[1]
+        y = np.append(y,b)     
+
+    return x, y
+
+def lhs_sample_anti(n_samples):
+    """
+    Takes random n_samples with the lhs method.
+    Returns array x and y.
+    """
+    x = np.array([])
+    y = np.array([])
+    x_anti = np.array([])
+    y_anti = np.array([])
+
+    # Makes the space of points which van be chosen from
+    space = Space([(-2.,1.), (-1.5, 1.5)])  
+
+    # Chooses which kind oh lhs will be used
+    lhs = Lhs(lhs_type="classic", criterion=None)
+
+    coordinates = 0
+    # Generates n_samples withhi the chosen space
+    coordinates = lhs.generate(space.dimensions, n_samples)
+    
+    # appends all x and y values to array
+    for coordinate in coordinates:
+        a = coordinate[0]
+        x = np.append(x,a)
+        x_anti = np.append(x_anti,a)
+        b = coordinate[1]
         y = np.append(y,b)
-        
-        if antithetic:
-            a_anti = 3.0 - (a + 2.0)
-            x = np.append(x, a_anti) 
-            b_anti = 3.0 - (b + 1.5)
-            y = np.append(y, b_anti)         
+        y_anti = np.append(y_anti,b)
+    
+        a_anti = 3.0 - (a + 2.0)
+        x_anti = np.append(x_anti, a_anti) 
+        b_anti = 3.0 - (b + 1.5)
+        y_anti = np.append(y_anti, b_anti)         
 
     return x, y, x_anti, y_anti
