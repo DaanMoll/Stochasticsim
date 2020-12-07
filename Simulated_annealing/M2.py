c76226418a86b7b0e29ad29eec5ecb42d44bdeac
Daan Moll <daanmoll@hotmail.com>
2020-12-07 12:42:54 +0100

    SA working

--- Simulated_annealing/M2.py
+++ Simulated_annealing/M2.py
@@ -17,11 +17,11 @@ def acceptance_probability(distance, new_distance, temperature):
 def simulated_annealing(distance, connections):
     fout = 0
 
-    start_temp = 2000
+    start_temp = 100
     current_distance = distance
 
     replace_cities = []
-    replace_connections = 5
+    replace_connections = 2
     max_iterations = 1000
 
     for iteration in range(max_iterations):
@@ -77,11 +77,10 @@ def simulated_annealing(distance, connections):
 
         # check if new solution is correct
         check_connections = copy.deepcopy(connections)
-        position = connections[0]
+        position = check_connections[0]
 
         while check_connections.count(position) > 0:
             next_position = position[1]
-
             check_connections.remove(position)
 
             for connection in check_connections:
@@ -92,10 +91,6 @@ def simulated_annealing(distance, connections):
             fout += 1
             connections = backup_connections
             continue
-            
-        # print("eind len conn", len(connections))
-        # print("current distance:", current_distance)
-        # print("new distance:", new_distance)
         
         if new_distance < current_distance:
             current_distance = new_distance
@@ -108,37 +103,15 @@ def simulated_annealing(distance, connections):
                 connections = backup_connections
 
     print("current:", current_distance)
-
-    for connection in connections:
-        plt.plot([cities[connection[0]][0], cities[connection[1]][0]], [cities[connection[0]][1], cities[connection[1]][1]])
-    
     print("fout van:", fout, "iterations:", max_iterations)
-    # plt.show()
 
+    return connections, current_distance
 
-if __name__ == "__main__":
-    # Choose TSP file
-    file_ = "eil51.tsp.txt"
-
-    # Initializes lists and dictionaries
-    cities = {}
+def nearest_neighbour(cities):
+    order = []
     connections = []
     total_distance = []
-    order = []
 
-    # Reads from data file and plots and saves all coordinates from cities
-    with open(f'TSP_data/{file_}', 'r') as reader:
-        counter = 0
-        for line in reader:
-            if line[0].isdigit():
-                counter+=1
-                new_line = line.split()
-                # print(new_line)
-                plt.plot(int(new_line[1]), int(new_line[2]), '.')
-                cities[counter] = (int(new_line[1]), int(new_line[2]))
-                
-    # Makes initial random connections between cities
-    # Checks the distances between cities and save them
     city_numbers = [i for i in range(1, counter+1, 1)]
 
     number_1 = np.random.choice(city_numbers)
@@ -154,11 +127,7 @@ if __name__ == "__main__":
         lowest_distance = 999999
 
         for number in city_numbers:
-            # if number_1 == number:
-            #     continue
-
             city2 = cities[number]
-
             distance = np.sqrt((city2[0] - city1[0])**2 + (city2[1] - city1[1])**2)
             
             if distance < lowest_distance:
@@ -183,9 +152,39 @@ if __name__ == "__main__":
     order.append(first_city)
     total_distance.append(distance)
 
-    print(len(connections))
-    print("Sum before sa:", np.sum(total_distance))
+    return connections, np.sum(total_distance)
 
-    simulated_annealing(np.sum(total_distance), connections)
+if __name__ == "__main__":
+    # Choose TSP file
+    file_ = "eil51.tsp.txt"
+
+    # Initializes lists and dictionaries
+    cities = {}
+
+    # Reads from data file and plots and saves all coordinates from cities
+    with open(f'TSP_data/{file_}', 'r') as reader:
+        counter = 0
+        for line in reader:
+            if line[0].isdigit():
+                counter+=1
+                new_line = line.split()
+                # print(new_line)
+                plt.plot(int(new_line[1]), int(new_line[2]), '.')
+                cities[counter] = (int(new_line[1]), int(new_line[2]))
+            
+
+    result = nearest_neighbour(cities)
+    connections = result[0]
+    start_distance = result[1]
+    print("Sum before sa:", start_distance)
+
+    result = simulated_annealing(np.sum(start_distance), connections)
+    connections = result[0]
+    sa_distance = result[1]
+    
+    if sa_distance < start_distance:
+        for connection in connections:
+            plt.plot([cities[connection[0]][0], cities[connection[1]][0]], [cities[connection[0]][1], cities[connection[1]][1]])
+        plt.show()
 
     
