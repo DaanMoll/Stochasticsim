ced907d3709f19b090f2e38f274c2e8392a19bd2
kamielgulpen <k.gulpen@hotmail.nl>
2020-12-08 15:10:43 +0100

    2-o

---
+++ Simulated_annealing/CS.py
@@ -0,0 +1,12 @@
+def cooling_schedule(start_temp, max_iterations, iteration, kind):
+    alpha = 1.5
+    if kind  == "Linear":
+        current_temp = start_temp/(1 + alpha*iteration)
+    elif kind == "Linear2":
+        current_temp = start_temp/(start_temp/max_iterations) * iteration
+    elif kind == "Log":
+        current_temp =  start_temp/(1 + alpha * (math.log(start_temp + iteration, 10)))
+    elif kind == "Exponential":
+        current_temp = start_temp*0.9**iteration
+    elif kind == "Quadratic":
+        current_temp =  start_temp/(1 + alpha * iteration**2)
