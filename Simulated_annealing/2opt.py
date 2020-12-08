3d3fd4cbdc87b93ea2a197773a8b3f0d9776fd34
Daan Moll <daanmoll@hotmail.com>
2020-12-08 16:22:45 +0100

    2opt plot

--- Simulated_annealing/2opt.py
+++ Simulated_annealing/2opt.py
@@ -1,5 +1,37 @@
+import matplotlib.pyplot as plt 
 import numpy as np
-from M import *
+from operator import itemgetter
+import copy
+import random
+import math
+
+def make_matrix(tsp_file):
+    """"
+    Creates an adjacency matrix based on the tsp file
+    """ 
+    # Extracting node coordinates from tsp file
+    node_list = []
+    with open(f"TSP_data/{tsp_file}.tsp.txt","r") as reader:
+        for line in reader:
+            line = line.strip("\n")
+            line = line.split()
+
+            if line[0].isdigit():
+                node_list.append([int(x) for x in line])
+
+    # Creating adjacency matrix
+    num_node = len(node_list)
+    adjacency_matrix = np.zeros((num_node,num_node))
+    for node1 in range(num_node):
+        for node2 in range(num_node):
+            if node1 != node2:
+                x = abs(node_list[node1][1] - node_list[node2][1])
+                y = abs(node_list[node1][2] - node_list[node2][2])
+                dist = np.sqrt(x**2+y**2)
+                adjacency_matrix[node1][node2] = dist
+
+    return adjacency_matrix
+
 
 def acceptance_probability(cost, temperature):
     """
@@ -29,12 +61,13 @@ def two_opt(route, cost_mat):
 
     return best
 
-def sa_two_opt(route, cost_mat):
-    start_temp = 10
+def sa_two_opt(route, cost_mat, distance):
     max_iterations = 1000
+    start_temp = 150
     accepted = 0
-
+    distances = []
     best = route
+    count = 0
 
     for iteration in range(max_iterations):
         for i in range(1, len(route) - 2):
@@ -48,12 +81,17 @@ def sa_two_opt(route, cost_mat):
                 if random.uniform(0, 1) < ap:
                     best[i:j] = best[j - 1:i - 1:-1]
                     accepted += 1
+                    distance += cost
+                    
+                distances.append(distance)
+                count += 1
+
         route = best
-    return best
+    return best, distances, count
 
 def calculate_cost(order, matrix):
     distance = 0
-
+    order = copy.deepcopy(order)
     first_city = order[0]
     current_city = order[0]
     order.pop(0)
@@ -69,26 +107,96 @@ def calculate_cost(order, matrix):
     
     return distance
 
+def nearest_neighbour(matrix):
+    order = []
+    total_distance = 0
+    city_numbers = [i for i in range(0, len(matrix), 1)]
+
+    number_1 = np.random.choice(city_numbers)
+    first_city = number_1
+
+    order.append(first_city)
+
+    city_numbers.remove(number_1)
+    number_2 = number_1
+
+    while len(city_numbers) != 0:
+        closest = math.inf
+
+        for number in city_numbers:
+            if number == number_1:
+                continue
+            distance = matrix[number_1][number]
+
+            if distance < closest:
+                closest = distance
+                number_2 = number
+        
+        if number_2 == number_1:
+            continue
+
+        distance = matrix[number_1][number_2]
+        total_distance += distance
+    
+        order.append(number_2)
+        city_numbers.remove(number_2)
+        number_1 = number_2
+
+    # end to begin
+    distance = matrix[number_2][first_city]
+    total_distance += distance
+    return order
+
 if __name__ == '__main__':
     tsp_file = "a280"
 
     matrix = make_matrix(tsp_file)
     
     nodes = len(matrix)
-    result = nearest_neighbour(matrix)
-    print("nn distance:", result[0])
-    
-    nn_route = result[1]
+    nn_route = nearest_neighbour(matrix)
+    distance_nn = calculate_cost(nn_route, matrix)
+    print("nn distance:", distance_nn)
     print("len nn", len(nn_route))
 
     cost_mat = list(matrix)
-    # best_route = sa_two_opt(nn_route, cost_mat)
-    # print(best_route)
+    result = sa_two_opt(nn_route, cost_mat, distance_nn)
+    best_route = result[0]
+    distances = result[1]
+    count = result[2]
     
-    best_route = [74, 76, 75, 73, 72, 71, 70, 69, 66, 68, 67, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 58, 62, 63, 64, 65, 84, 85, 115, 112, 86, 83, 82, 87, 111, 108, 107, 109, 110, 113, 114, 116, 117, 61, 60, 59, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 21, 24, 22, 23, 13, 14, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 276, 275, 274, 273, 272, 271, 270, 15, 16, 17, 132, 131, 18, 19, 130, 129, 20, 128, 127, 126, 125, 124, 123, 122, 121, 120, 153, 154, 152, 155, 151, 119, 118, 156, 157, 158, 159, 174, 173, 172, 106, 105, 104, 103, 102, 101, 91, 90, 89, 88, 81, 80, 79, 78, 77, 94, 95, 96, 93, 92, 97, 98, 99, 100, 168, 169, 171, 170, 167, 166, 165, 164, 163, 162, 161, 160, 180, 175, 176, 150, 177, 149, 178, 179, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 193, 194, 195, 200, 199, 201, 202, 203, 204, 205, 207, 252, 251, 208, 209, 206, 211, 210, 213, 212, 215, 214, 217, 216, 219, 220, 221, 218, 222, 223, 224, 225, 226, 227, 228, 229, 250, 249, 246, 244, 245, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 243, 242, 241, 1, 0, 279, 2, 278, 277, 247, 248, 255, 254, 253, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 133, 134, 135, 136, 137, 138, 148, 147, 139, 140, 141, 146, 145, 142, 143, 144, 198, 197, 196, 192]
     print("na sa cost:", calculate_cost(best_route, matrix))
-    print("best route len:", len(best_route))
+    
+    # start plot
+    # Initializes lists and dictionaries
+    cities = {}
 
-    best_2opt = two_opt(nn_route, cost_mat)
-    print("hoi", best_2opt)
-    print("2opt cost:", calculate_cost(best_2opt, cost_mat))
+    # Reads from data file and plots and saves all coordinates from cities
+    with open(f"TSP_data/{tsp_file}.tsp.txt","r") as reader:
+        counter = 0
+        for line in reader:
+            line = line.strip("\n")
+            line = line.split()
+    
+            if line[0].isdigit():
+                # counter+=1
+                plt.plot(int(line[1]), int(line[2]), '.')
+                # plt.annotate(counter, [int(line[1]), int(line[2])], fontsize=8)
+
+                cities[counter] = (int(line[1]), int(line[2]))
+                counter+=1
+
+    route = best_route
+    for i in range(len(route) - 1):
+        city1 = cities[route[i]]
+        city2 = cities[route[i+1]]
+        plt.plot([city1[0], city2[0]], [city1[1], city2[1]])
+
+    city1 = cities[route[-1]]
+    city2 = cities[route[0]]
+    plt.plot([city1[0], city2[0]], [city1[1], city2[1]])
+
+    plt.show()
+    count = np.linspace(0, count - 1, count)
+
+    plt.plot(count, distances)
+    plt.show()
